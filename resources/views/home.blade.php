463ce6ad9adc52ecb8995cfeb488be67355ae523
Luce <boama561@gmail.com>
2025-04-26 12:14:51 +0000

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel application</title>
+</head>
+<body>
+    <h1>Bienvenue sur notre application {{ $app_name }}</h1>
+    <x-menu />
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    $app_name = "Laravel 10";
+    return view('home', compact('app_name'));
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
